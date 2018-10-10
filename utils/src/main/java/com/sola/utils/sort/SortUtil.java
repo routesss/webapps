eb42ce4eccc37bffe090739ad9bb6ef1705e3673
routesss <1301524857@qq.com>
2018-10-10 16:19:51 +0800

    排序测试

--- utils/src/main/java/com/sola/utils/sort/SortUtil.java
+++ utils/src/main/java/com/sola/utils/sort/SortUtil.java
@@ -3,8 +3,29 @@ package com.sola.utils.sort;
 /**
  * 排序相关
  */
+import java.util.Arrays;
+
 public class SortUtil {
 
-
+    /**
+     * 选择排序
+     * @param buffer
+     */
+    public static void selectionSorting(int buffer[]){
+        int minIndex = 0 ;
+        for(int i = 0; i < buffer.length; i++){
+            minIndex = i ;
+            for(int index = i; index < buffer.length; index++){
+                if(buffer[minIndex] > buffer[index]){
+                    minIndex = index ;
+                }
+            }
+            if (i != minIndex){
+                buffer[i] = buffer[i] + buffer[minIndex] ;
+                buffer[minIndex] = buffer[i] - buffer[minIndex] ;
+                buffer[i] = buffer[i] - buffer[minIndex] ;
+            }
+        }
+    }
 
 }
--- utils/src/main/test/ApplicationTEST.java
+++ utils/src/main/test/ApplicationTEST.java
@@ -1,5 +1,8 @@
+import com.sola.utils.sort.SortUtil;
 import com.sola.utils.sort.find.FindUtil;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 /**
  * 测试类
@@ -11,6 +14,14 @@ public class ApplicationTEST {
         int num[] = new int[]{1, 3, 7, 15, 23, 45, 58, 78, 85, 95} ;
 
         System.out.println(FindUtil.twoPoint(num, 23));
+    }
+
+    @Test
+    public void demo2(){
+        int num[] = new int[]{98,55,15,69,87,25,36,84,1,65} ;
+
+        SortUtil.selectionSorting(num);
+        System.out.println(Arrays.toString(num));
     }
 
 }
